6dd10db375a671fb4b826d843413e1f4d56b7912
unknown <mattimadsen@gmail.com>
2023-09-26 23:42:20 +0000

    WOWOWOD

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+var health = 10
 const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
 
--- scenes/frog.gd
+++ scenes/frog.gd
@@ -3,33 +3,37 @@ extends CharacterBody2D
 # Create variables
 var player
 var chase = false
-var SPEED = 40
+var SPEED = 60
 var JUMP_VELOCITY = -200.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	get_node("AnimatedSprite2D").play("idle")
 
 func _physics_process(delta):
 	# Frog movement
 	velocity.y += gravity * delta
 	
 	if chase == true:
-		player = get_node("../../player/player")
-		
-		get_node("AnimatedSprite2D").play("jump")
-		
-		var direction = (player.position - self.position).normalized()
-		if direction.x > 0:
-			# right
-			get_node("AnimatedSprite2D").flip_h = true
-		else:
-			# left
-			get_node("AnimatedSprite2D").flip_h = false
+		if get_node("AnimatedSprite2D").animation != "death":
+			get_node("AnimatedSprite2D").play("jump")
+			player = get_node("../../player/player")
 			
-		velocity.x = direction.x * SPEED
+			var direction = (player.position - self.position).normalized()
+			if direction.x > 0:
+				# right
+				get_node("AnimatedSprite2D").flip_h = true
+			else:
+				# left
+				get_node("AnimatedSprite2D").flip_h = false
+				
+			velocity.x = direction.x * SPEED
 	else:
-		get_node("AnimatedSprite2D").play("idle")
-		velocity.x = 0
+		if get_node("AnimatedSprite2D").animation != "death":
+			get_node("AnimatedSprite2D").play("idle")
+			velocity.x = 0
 		
 		
 	move_and_slide()
@@ -42,3 +46,15 @@ func _on_player_detection_body_entered(body):
 func _on_player_detection_body_exited(body):
 	if body.name == "player":
 		chase = false
+
+
+
+
+
+func _on_player_death_body_entered(body):
+	if body.name == "player":
+		chase = false
+		get_node("AnimatedSprite2D").play("death")
+		await get_node("AnimatedSprite2D").animation_finished
+		
+		self.queue_free()
